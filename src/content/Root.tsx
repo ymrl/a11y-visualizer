9d79893c62f6c48cd3d396855ec39c02a8da3ea8
ymrl <ymrl@ymrl.net>
2024-04-14 08:29:53 +0900

    :+1: add showing aria live region feature

--- src/content/Root.tsx
+++ src/content/Root.tsx
@@ -4,12 +4,12 @@ import { Settings, Message } from "../types";
 import { ElementMeta } from "./types";
 import { MetaList } from "./components/MetaList";
 import { injectRoot } from "./injectRoot";
+import { Announcements } from "./components/Announcements";
 
 export const Root = ({ parent }: { parent: Element }) => {
   const [metaList, setMetaList] = React.useState<ElementMeta[]>([]);
   const [width, setWidth] = React.useState<number>(0);
   const [height, setHeight] = React.useState<number>(0);
-
   const [settings, setSettings] = React.useState<Settings>({
     accessibilityInfo: false,
     image: true,
@@ -19,6 +19,7 @@ export const Root = ({ parent }: { parent: Element }) => {
     ariaHidden: true,
   });
   const containerRef = React.useRef<HTMLDivElement>(null);
+  const announcementsRef = React.useRef<HTMLDivElement>(null);
   const framesRef = React.useRef<Window[]>([]);
   const dialogsRef = React.useRef<Element[]>([]);
   const popoversRef = React.useRef<Element[]>([]);
@@ -56,20 +57,66 @@ export const Root = ({ parent }: { parent: Element }) => {
     popoversRef.current = Array.from(popovers);
   }, []);
 
+  const liveRegionsRef = React.useRef<Element[]>([]);
+  const liveRegionObserverRef = React.useRef<MutationObserver | null>(null);
+  const [announcements, setAnnouncements] = React.useState<string[]>([]);
+
+  React.useEffect(() => {
+    const observer = new MutationObserver((records) => {
+      const content = records
+        .map((r) => (r.target.textContent || "").trim())
+        .filter(Boolean);
+      setAnnouncements((prev) => [...prev, ...content]);
+      content.forEach((c) => {
+        setTimeout(
+          () => {
+            setAnnouncements((prev) => {
+              const idx = prev.indexOf(c);
+              return idx === -1
+                ? prev
+                : [...prev.slice(0, idx), ...prev.slice(idx + 1)];
+            });
+          },
+          Math.min(c.length * 500, 30000),
+        );
+      });
+    });
+    liveRegionObserverRef.current = observer;
+    return () => observer.disconnect();
+  }, []);
+
+  const observeLiveRegion = React.useCallback((el: Element) => {
+    if (!liveRegionObserverRef.current) {
+      return;
+    }
+    const liveRegions = el.querySelectorAll(
+      "output, [role='status'], [role='alert'], [role='log'], [aria-live]:not([aria-live='off'])",
+    );
+    [...liveRegions].forEach((el) => {
+      if (!liveRegionsRef.current.includes(el)) {
+        liveRegionObserverRef.current?.observe(el, {
+          subtree: true,
+          childList: true,
+          characterData: true,
+        });
+      }
+    });
+    liveRegionsRef.current = Array.from(liveRegions);
+  }, []);
+
   const updateInfo = React.useCallback(() => {
     injectToFrames(parent);
-
+    observeLiveRegion(parent);
     if (settings.accessibilityInfo) {
       injectToDialogs(parent);
       const { elements, rootHeight, rootWidth } = collectElements(
         parent,
-        containerRef.current
-          ? [
-              containerRef.current,
-              ...popoversRef.current,
-              ...dialogsRef.current,
-            ]
-          : [],
+        [
+          containerRef.current,
+          announcementsRef.current,
+          ...popoversRef.current,
+          ...dialogsRef.current,
+        ].filter((el): el is Element => !!el),
       );
 
       setMetaList(elements);
@@ -80,7 +127,7 @@ export const Root = ({ parent }: { parent: Element }) => {
       setHeight(0);
       setMetaList([]);
     }
-  }, [injectToFrames, settings.accessibilityInfo, injectToDialogs, parent]);
+  }, [injectToFrames, parent, settings, observeLiveRegion, injectToDialogs]);
 
   React.useEffect(() => {
     chrome.storage.local.get("settings", (data) => {
@@ -119,12 +166,15 @@ export const Root = ({ parent }: { parent: Element }) => {
   }, [settings, updateInfo]);
 
   return (
-    <MetaList
-      list={metaList}
-      settings={settings}
-      ref={containerRef}
-      width={width}
-      height={height}
-    />
+    <>
+      <MetaList
+        list={metaList}
+        settings={settings}
+        ref={containerRef}
+        width={width}
+        height={height}
+      />
+      <Announcements contents={announcements} ref={announcementsRef} />
+    </>
   );
 };

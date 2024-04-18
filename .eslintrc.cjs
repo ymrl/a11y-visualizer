ac685c146c14261a63affa829dc9bcdc12df453f
ymrl <ymrl@ymrl.net>
2024-04-19 00:29:46 +0900

    :green_heart: add no-console eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,6 +11,7 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
+    "no-console": "error",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },

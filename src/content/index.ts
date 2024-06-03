374d2e6c9470ef7da946a1dfc3a0e7a90b24fce8
ymrl <ymrl@ymrl.net>
2024-06-04 00:02:03 +0900

    :bug: avoid top-level await

--- src/content/index.ts
+++ src/content/index.ts
@@ -2,20 +2,22 @@ import { loadEnabled } from "../enabled";
 import { injectRoot } from "./injectRoot";
 import { SettingsMessage } from "../settings";
 
-let injected = false;
-const enabled = await loadEnabled();
-if (enabled) {
-  injectRoot(window, window.document.body);
-  injected = true;
-}
-
-const listener = (message: SettingsMessage) => {
-  if (message.type !== "updateEnabled") return;
-  if (message.enabled && !injected) {
+(async () => {
+  let injected = false;
+  const enabled = await loadEnabled();
+  if (enabled) {
     injectRoot(window, window.document.body);
     injected = true;
-  } else {
-    injected = false;
   }
-};
-chrome.runtime.onMessage.addListener(listener);
+
+  const listener = (message: SettingsMessage) => {
+    if (message.type !== "updateEnabled") return;
+    if (message.enabled && !injected) {
+      injectRoot(window, window.document.body);
+      injected = true;
+    } else {
+      injected = false;
+    }
+  };
+  chrome.runtime.onMessage.addListener(listener);
+})();

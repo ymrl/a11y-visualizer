d62e62f3f6fe2ba5294832cefa4ff2645712c1e7
ymrl <ymrl@ymrl.net>
2024-06-09 20:26:25 +0900

    :rocket: simplify ishidden

--- src/content/dom/isHidden.ts
+++ src/content/dom/isHidden.ts
@@ -1,24 +1,16 @@
 export const isHidden = (el: Element): boolean => {
-  // 親要素
-  const parent = el.parentElement;
-  // 親要素が存在しない場合は非表示である
-  if (!parent) return true;
+  if (el instanceof HTMLElement && !el.offsetParent) return true;
+  if (el.matches("details:not([open]) > *:not(summary)")) return true;
+
   const d = el.ownerDocument;
   const w = d.defaultView;
   if (!w) return true;
-  // elementのCSSスタイル
-  const style = w.getComputedStyle(el);
-  // 自身の表示状態が非表示である
-  if (style.display === "none" || style.visibility === "hidden") return true;
-  // 親要素がbodyならば表示状態である
-  if (parent === d.body) return false;
-  // 親要素がdetails要素であり、かつ自身がsummary要素でない場合、かつdetails要素が閉じている場合は非表示である
-  if (
-    parent.tagName === "DETAILS" &&
-    el.tagName !== "SUMMARY" &&
-    !parent.hasAttribute("open")
-  )
-    return true;
-  // 先祖要素が非表示である場合は非表示である
-  return isHidden(parent);
+  let t = el;
+  while (t && t !== el.ownerDocument.body) {
+    const style = w.getComputedStyle(t);
+    if (style.visibility === "hidden") return true;
+    if (!t.parentElement) return true;
+    t = t.parentElement;
+  }
+  return false;
 };

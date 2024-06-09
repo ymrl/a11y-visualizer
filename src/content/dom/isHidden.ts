3993c1579a35f79c69c7211ec64a560bf8b80575
ymrl <ymrl@ymrl.net>
2024-06-10 00:20:39 +0900

    :heart: not to use offsetParent and add hidden attribute

--- src/content/dom/isHidden.ts
+++ src/content/dom/isHidden.ts
@@ -1,6 +1,6 @@
 export const isHidden = (el: Element): boolean => {
-  if (el instanceof HTMLElement && !el.offsetParent) return true;
   if (el.matches("details:not([open]) > *:not(summary)")) return true;
+  if (el.matches("[hidden]")) return true;
 
   const d = el.ownerDocument;
   const w = d.defaultView;
@@ -8,7 +8,7 @@ export const isHidden = (el: Element): boolean => {
   let t = el;
   while (t && t !== el.ownerDocument.body) {
     const style = w.getComputedStyle(t);
-    if (style.visibility === "hidden") return true;
+    if (style.display === "none" || style.visibility === "hidden") return true;
     if (!t.parentElement) return true;
     t = t.parentElement;
   }

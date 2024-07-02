a01e17da449817920894d17a939ab9b14e466a13
ymrl <ymrl@freee.co.jp>
2024-07-02 20:24:49 +0900

    :+1: add content-visibility: hidden to and remove hidden attr from isHidden

--- src/content/dom/isHidden.ts
+++ src/content/dom/isHidden.ts
@@ -5,7 +5,6 @@ export const isHidden = (el: Element): boolean => {
     }
     return true;
   }
-  if (el.matches("[hidden]")) return true;
 
   const d = el.ownerDocument;
   const w = d.defaultView;
@@ -13,7 +12,12 @@ export const isHidden = (el: Element): boolean => {
   let t = el;
   while (t && t !== el.ownerDocument.body) {
     const style = w.getComputedStyle(t);
-    if (style.display === "none" || style.visibility === "hidden") return true;
+    if (
+      style.display === "none" ||
+      style.visibility === "hidden" ||
+      style.getPropertyValue("content-visibility") === "hidden"
+    )
+      return true;
     if (!t.parentElement) return true;
     t = t.parentElement;
   }

4c6d8a86a96ff77204e3c821ce7a71f3b41737f1
ymrl <ymrl@ymrl.net>
2024-07-27 14:31:35 +0900

    :bug: fix isDefaultSize to fit various inpu types

--- src/dom/isDefaultSize.test.ts
+++ src/dom/isDefaultSize.test.ts
@@ -27,4 +27,40 @@ describe("isDefaultSize", () => {
     document.body.appendChild(el);
     expect(isDefaultSize(el)).toBe(false);
   });
+
+  test("input type not specified", () => {
+    const el = document.createElement("input");
+    document.body.appendChild(el);
+    expect(isDefaultSize(el)).toBe(true);
+  });
+
+  [
+    "text",
+    "search",
+    "tel",
+    "email",
+    "password",
+    "date",
+    "month",
+    "week",
+    "time",
+    "datetime-local",
+    "number",
+    "range",
+    "color",
+    "checkbox",
+    "radio",
+    "file",
+    "submit",
+    // "image",
+    "reset",
+    "button",
+  ].forEach((type) => {
+    test(`default input type ${type}`, () => {
+      const el = document.createElement("input");
+      el.setAttribute("type", type);
+      document.body.appendChild(el);
+      expect(isDefaultSize(el)).toBe(true);
+    });
+  });
 });

2cbf4f975f84911559b37d5bbd3fe0c814461781
ymrl <ymrl@ymrl.net>
2025-09-14 08:45:21 +0900

    Remove `t` prop from layout

---
+++ apps/website/src/layouts/SiteLayout.lang.ts
@@ -0,0 +1,32 @@
+export const en = {
+  title: "Accessibility Visualizer",
+  description:
+    "A browser extension that displays accessibility-related annotations on web pages to assist developers.",
+  hero: {
+    imageAlt:
+      "Accessibility Visualizer's icon (Cartoon eyes on colorful blocks)",
+  },
+  footer: { copyright: "© 2024 ymrl (MIT License)", github: "GitHub" },
+} as const;
+
+export const ja = {
+  title: "Accessibility Visualizer",
+  description:
+    "Webページの上にアクセシビリティに関係する注釈を表示し、開発者を支援するブラウザ拡張機能です。",
+  hero: {
+    imageAlt:
+      "Accessibility Visualizerのアイコン (カラフルなブロックの上に描かれた漫画の目)",
+  },
+  footer: { copyright: "© 2024 ymrl (MIT License)", github: "GitHub" },
+} as const;
+
+export const ko = {
+  title: "Accessibility Visualizer",
+  description:
+    "웹 페이지에 접근성 관련 주석을 표시하여 개발자를 지원하는 브라우저 확장 프로그램입니다.",
+  hero: {
+    imageAlt:
+      "Accessibility Visualizer 아이콘 (화려한 블록 위에 그려진 만화 눈)",
+  },
+  footer: { copyright: "© 2024 ymrl (MIT License)", github: "GitHub" },
+} as const;
